12c7838220084b8e5a8cde056ab767b18dfe70f9
Urs Schmidt <uschmidt@c3a1c1.42berlin.de>
2024-12-03 13:00:26 +0100

    feat: some proper output

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,30 @@
 /*   By: uschmidt <uschmidt@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 10:06:14 by uschmidt          #+#    #+#             */
-/*   Updated: 2024/12/03 10:48:40 by uschmidt         ###   ########.fr       */
+/*   Updated: 2024/12/03 12:59:05 by uschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int	get_newline_chr(char *str)
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	char	*dest;
+
+	i = 0;
+	dest = (char *)s;
+	while (i < n)
+		dest[i++] = 0;
+}
+
+int	get_newline_chr(char *str, int buff_len)
 {
 	int	i;
 
 	i = 0;
-	while (i < BUFFER_SIZE && str[i] != '\n')
+	while (i < buff_len && str[i] != '\n')
 		i++;
 	return (i);
 }
@@ -31,7 +42,9 @@ char	*extend_line(char *line, int size)
 	line_length = 0;
 	if (line)
 		line_length = ft_strlen(line);
-	linebuffer = (char *)malloc(sizeof(char) * (line_length + size));
+	linebuffer = (char *)malloc(sizeof(char) * (line_length + size + 1));
+	if (!linebuffer)
+		return (NULL);
 	if (line)
 	{
 		ft_memmove(linebuffer, line, line_length);
@@ -43,27 +56,31 @@ char	*extend_line(char *line, int size)
 char	*get_line(int fd, char *buffer)
 {
 	int			i;
+	int			buff_len;
 	char		*line;
 	ssize_t		read_bytes;
 
-	write(1, "DEBUGGER_1\n", 11);
-	i = get_newline_chr(buffer);
+	buff_len = ft_strlen(buffer);
+	i = get_newline_chr(buffer, buff_len);
 	line = NULL;
-	while (i == BUFFER_SIZE)
+	while (i == buff_len)
 	{
 		line = extend_line(line, i);
-		line = ft_memmove(line + ft_strlen(line), buffer, i);
-		line[i + ft_strlen(line) + 1] = '\0';
-		read_bytes = read(fd, buffer, i);
-		i = get_newline_chr(buffer);
+		ft_memmove(line + ft_strlen(line), buffer, i);
+		line[i + ft_strlen(line)] = '\0';
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (read_bytes <= 0)
+			break ;
+		buff_len = ft_strlen(buffer);
+		i = get_newline_chr(buffer, buff_len);
 	}
 	line = extend_line(line, i);
-	write(1, "DEBUGGER_2\n", 11);
-	line = ft_memmove(line + ft_strlen(line), buffer, i);
-	write(1, "DEBUGGER_3\n", 11);
-	line[i + ft_strlen(line) + 1] = '\0';
-	ft_memmove(buffer, buffer + BUFFER_SIZE - i, i);
-	write(1, "DEBUGGER_4\n", 11);
+	ft_memmove(line + ft_strlen(line), buffer, i);
+	line[i + ft_strlen(line)] = '\0';
+	ft_memmove(buffer, buffer + i + 1, BUFFER_SIZE - i);
+	write(1, buffer, BUFFER_SIZE);
+	printf("\n/\\ BUFFER /\\\n");
+	ft_bzero(buffer + (BUFFER_SIZE - i), i);
 	return (line);
 }
 
@@ -81,5 +98,7 @@ char	*get_next_line(int fd)
 		if (read_bytes <= 0)
 			return (NULL);
 	}
+	if (buffer[0] == '\0')
+		return (NULL);
 	return (get_line(fd, buffer));
 }
